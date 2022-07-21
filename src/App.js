b69196978890558de3cbf978c3cb70b8ca498a11
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-21 14:21:15 +0700

    [FEATURE] Add SimpleJsonPlaceHolderView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import JsonPlaceHolderView from "./features/jsonPlaceHolder/JsonPlaceHolderView";
 import useJsonPlaceHolderController from "./features/jsonPlaceHolder/useJsonPlaceHolderController";
+import SimpleJsonPlaceHolderView from "./features/jsonPlaceHolder/SimpleJsonPlaceHolderView";
 
 // npm install axios --save
 
 function App() {
     return (
-        <JsonPlaceHolderView controller={useJsonPlaceHolderController}/>
+        // <JsonPlaceHolderView controller={useJsonPlaceHolderController}/>
+        <SimpleJsonPlaceHolderView/>
     )
 }
 
