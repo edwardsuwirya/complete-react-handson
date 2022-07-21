58e05a71f3ce3136cae4409a8c02d775071577d9
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-21 12:24:07 +0700

    [IMPROVE] Change prop name with events dan data type is object

--- src/features/jsonPlaceHolder/JsonPlaceHolderView.js
+++ src/features/jsonPlaceHolder/JsonPlaceHolderView.js
@@ -2,7 +2,7 @@ import JsonPlaceHolderContent from "./components/JsonPlaceHolderContent";
 
 const JsonPlaceHolderView = ({controller}) => {
     const {viewState, createPost} = controller();
-    return <JsonPlaceHolderContent state={viewState} onCreatePost={createPost}/>;
+    return <JsonPlaceHolderContent state={viewState} events={{createPost}}/>;
 }
 
 export default JsonPlaceHolderView;
--- src/features/jsonPlaceHolder/components/JsonPlaceHolderContent.js
+++ src/features/jsonPlaceHolder/components/JsonPlaceHolderContent.js
@@ -1,11 +1,11 @@
 import withViewState from "../../../shared/components/withViewState";
 
-const JsonPlaceHolderContent = ({data, onCreatePost}) => {
+const JsonPlaceHolderContent = ({data, events: {createPost}}) => {
     return (
         <div>
             <h1>{data.title}</h1>
             <p>{data.body}</p>
-            <button onClick={onCreatePost}>Create Post</button>
+            <button onClick={createPost}>Create Post</button>
         </div>
     )
 }
