fcf3eb412bfc06f66b0ae48653fb267ce4dc085a
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-23 09:38:38 +0700

    [IMPROVE] new group apiClient

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {DepsProvider} from "./shared/depContext";
-import ApiClientFactory from "./shared/apiClientFactory";
-import clientInstance from "./shared/axiosClient";
+import ApiClientFactory from "./shared/apiClient/apiClientFactory";
+import clientInstance from "./shared/apiClient/apiClientInstanceFactory";
 
 //create .env file in the root of the project
 //set environment variables starting with REACT_APP_
