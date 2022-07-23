9ce882f5d35e769aeb0c56333f9704c6bb15ce9a
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-23 09:38:38 +0700

    [IMPROVE] new group apiClient

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRouter from "./navigation/AppRouter";
-import ApiClientFactory from "./shared/apiClientFactory";
-import clientInstance from "./shared/axiosClient";
 import {DepsProvider} from "./shared/depContext";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import AppRouter from "./navigation/AppRouter";
 import loginService from "./services/loginService/loginService";
 import jsonPlaceHolderService from "./services/jsonPlaceHolderService/jsonPlaceHolderService";
+import ApiClientFactory from "./shared/apiClient/apiClientFactory";
+import clientInstance from "./shared/apiClient/apiClientInstanceFactory";
 
 //create .env file in the root of the project
 //set environment variables starting with REACT_APP_
