5251dd5321328bc1f2af272cd1dd63245b02aee8
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-25 14:44:47 +0700

    [FIX] Move BrowserRouter and AuthProvider to index [IMPROVE] configure redux store with preloaded state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRouter from "./navigation/AppRouter";
 import {DepsProvider} from "./shared/depContext";
 import {Provider} from "react-redux";
-import {store} from "./store";
+import AppRouter from "./navigation/AppRouter";
 import loginService from "./services/loginService/loginService";
 import jsonPlaceHolderService from "./services/jsonPlaceHolderService/jsonPlaceHolderService";
 import ApiClientFactory from "./shared/apiClient/apiClientFactory";
 import {clientInstance, jsonPlaceHolderClientInstance} from "./shared/apiClient/apiClientInstanceFactory";
+import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "./shared/hook/useAuth";
+import {setupStore} from "./store";
 
 //create .env file in the root of the project
 //set environment variables starting with REACT_APP_
 //access it by process.env.REACT_APP_... in components
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const samplePreloadedState = {
+//     jsonPlaceHolder: {
+//         postings: [{title: 'Hello World 2', body: 'Hello 2'}],
+//     }
+// }
+// const store = setupStore(samplePreloadedState);
+const store = setupStore();
 root.render(
     <React.StrictMode>
         <Provider store={store}>
@@ -26,7 +36,11 @@ root.render(
                     loginService, jsonPlaceHolderService
                 }}
             >
-                <AppRouter/>
+                <BrowserRouter>
+                    <AuthProvider>
+                        <AppRouter/>
+                    </AuthProvider>
+                </BrowserRouter>
             </DepsProvider>
         </Provider>
     </React.StrictMode>
--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -1,38 +1,33 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {AuthProvider} from "../shared/hook/useAuth";
-import LoginPageView from "../features/login/views/LoginPageView";
+import {Route, Routes} from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import App from "../App";
-import JsonPlaceHolderView from "../features/jsonPlaceHolder/views/JsonPlaceHolderView";
 import useLoginPageController from "../features/login/views/useLoginPageController";
 import useJsonPlaceHolderController from "../features/jsonPlaceHolder/views/useJsonPlaceHolderController";
+import JsonPlaceHolderView from "../features/jsonPlaceHolder/views/JsonPlaceHolderView";
 import JsonPlaceHolderListView from "../features/jsonPlaceHolder/views/JsonPlaceHolderListView";
+import LoginPageView from "../features/login/views/LoginPageView";
 
 const AppRouter = () => {
     return (
-        <BrowserRouter>
-            <AuthProvider>
-                <Routes>
-                    <Route index element={<LoginPageView controller={useLoginPageController}/>}/>
-                    <Route element={<ProtectedRoute/>}>
-                        <Route path="main" element={<App/>}>
-                            <Route path="jsonplaceholder"
-                                   element={<JsonPlaceHolderView controller={useJsonPlaceHolderController}/>}/>
-                            <Route path="jsonplaceholderList"
-                                   element={<JsonPlaceHolderListView/>}/>
-                        </Route>
-                    </Route>
-                    <Route
-                        path="*"
-                        element={
-                            <main style={{padding: "1rem"}}>
-                                <p>Oopss... page not found</p>
-                            </main>
-                        }
-                    />
-                </Routes>
-            </AuthProvider>
-        </BrowserRouter>
+        <Routes>
+            <Route index element={<LoginPageView controller={useLoginPageController}/>}/>
+            <Route element={<ProtectedRoute/>}>
+                <Route path="main" element={<App/>}>
+                    <Route path="jsonplaceholder"
+                           element={<JsonPlaceHolderView controller={useJsonPlaceHolderController}/>}/>
+                    <Route path="jsonplaceholderList"
+                           element={<JsonPlaceHolderListView/>}/>
+                </Route>
+            </Route>
+            <Route
+                path="*"
+                element={
+                    <main style={{padding: "1rem"}}>
+                        <p>Oopss... page not found</p>
+                    </main>
+                }
+            />
+        </Routes>
     )
 }
 
